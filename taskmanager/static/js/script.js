baf3aab6b76a67b03e47eaf006abd840b08a8ab2
jmarcosdias <jmsdias6@gmail.com>
2022-04-10 19:26:50 +0000

    Add basic tasks page

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
+
+//   document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.sidenav');
+//     var instances = M.Sidenav.init(elems, options);
+//   });
