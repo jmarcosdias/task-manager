5db132808ccfaf13917ad4950aed2c88f18a7a96
jmarcosdias <jmsdias6@gmail.com>
2022-04-18 16:13:35 +0000

    Add and read tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -7,3 +7,21 @@ document.addEventListener('DOMContentLoaded', function() {
 //     var elems = document.querySelectorAll('.sidenav');
 //     var instances = M.Sidenav.init(elems, options);
 //   });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    format: "dd mmmm, yyyy",
+    i18n: { done: "Select" }
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var selects = document.querySelectorAll('select');
+  M.FormSelect.init(selects);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var collapsibles = document.querySelectorAll('.collapsible');
+  var collapsiblesInstances = M.Collapsible.init(collapsibles);
+});
